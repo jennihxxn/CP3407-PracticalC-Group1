ee176bb586955d96d1e653e49ea4f759e8050df4
jihun9812 <jihun.kim1@my.jcu.edu.au>
2025-06-04 19:33:30 +0800

    Add files via upload
    I've done with all 3files and 2 lefts now.

---
+++ App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import RegisterForm from './components/RegisterForm';
+
+function App() {
+  return (
+    <div>
+      <h1>Gym Management System</h1>
+      <RegisterForm />
+    </div>
+  );
+}
+
+export default App;
